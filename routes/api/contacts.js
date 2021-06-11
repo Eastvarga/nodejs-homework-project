ad771ce76f3afc6c591e2fd304c2b64763b4d73e
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-11 20:36:23 +0300

    routs update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact
+} = require('../../model/index')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await listContacts()
+  res.status(200)
+  res.json({ message: contacts })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contact = await getContactById(req.params.contactId)
+  res.status(200)
+  res.json({ message: contact, status: 'success' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  addContact(req.body)
+  res.json({ message: 'contact added' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
