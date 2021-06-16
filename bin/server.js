8c32431afd038adafc2978b2690a74d468e74ad4
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-16 22:14:13 +0300

    connection to db done and work

--- bin/server.js
+++ bin/server.js
@@ -3,25 +3,21 @@ const MongoClient = require('mongodb').MongoClient
 const app = require('../app')
 
 const PORT = process.env.PORT || 3000
-console.log('process.env.MONGO_URL', process.env.MONGO_URL)
 const start = async () => {
-  try {
-    const client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true
-    })
-    const db = client.db()
+  const client = await MongoClient.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  const db = client.db('db-contacts')
 
-    const Contacts = db.collection('contacts')
-    const contacts = await Contacts.find({})
-    console.log(contacts)
-    app.listen(PORT, (err) => {
-      if (err) {
-        console.error('Error at server lanch', err)
-      }
-      console.log(`Server running. Use our API on port: ${PORT}`)
-    })
-  } catch (err) {
-    console.error('Error at try catch loop', err)
-  }
+  const Contacts = db.collection('contacts')
+  const contacts = await Contacts.find({}).toArray()
+  console.log(contacts)
+  app.listen(PORT, (err) => {
+    if (err) {
+      console.error('Error at server lanch', err)
+    }
+    console.log(`Server running. Use our API on port: ${PORT}`)
+  })
 }
 start()
