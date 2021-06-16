982440ba695c1976a617689fde0c8a6655b6e984
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-16 21:39:58 +0300

    start learn mongo DB

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,27 @@
+const MongoClient = require('mongodb').MongoClient
+
 const app = require('../app')
 
 const PORT = process.env.PORT || 3000
+console.log('process.env.MONGO_URL', process.env.MONGO_URL)
+const start = async () => {
+  try {
+    const client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true
+    })
+    const db = client.db()
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+    const Contacts = db.collection('contacts')
+    const contacts = await Contacts.find({})
+    console.log(contacts)
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.error('Error at server lanch', err)
+      }
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  } catch (err) {
+    console.error('Error at try catch loop', err)
+  }
+}
+start()
