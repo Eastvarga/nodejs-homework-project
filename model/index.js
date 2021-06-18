2708a060cc4bb5f64deebc91a0b8668172b4689a
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-18 23:10:24 +0300

    create GET contact by ID

--- model/index.js
+++ model/index.js
@@ -1,12 +1,13 @@
-const { connectMongo } = require('../db/connection')
-
+const { connectMongo, getCollections } = require('../db/connection')
+const ObjectId = require('mongodb').ObjectId
 const path = require('path')
 const fs = require('fs/promises')
+const { ObjectID } = require('mongodb')
 const contactsPath = path.resolve('./model/contacts.json')
 
 const listContacts = async () => {
   try {
-    const { Contacts } = await connectMongo()
+    const { Contacts } = await getCollections()
     const contacts = await Contacts.find({}).toArray()
     return contacts
   } catch (error) {
@@ -16,9 +17,11 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   try {
-    const data = await fs.readFile(contactsPath, 'utf8')
-    const contacts = JSON.parse(data)
-    const contact = contacts.find((elem) => elem.id === Number(contactId))
+    const { Contacts } = await getCollections()
+    const contact = await Contacts.findOne({
+      _id: new ObjectId(contactId)
+    })
+
     if (!contact) {
       return false
     }
