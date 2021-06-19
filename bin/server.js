7de703279d96683172d8fc6467c5cef23e1c4a34
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-19 16:35:46 +0300

    connection error handle

--- bin/server.js
+++ bin/server.js
@@ -3,12 +3,16 @@ const { connectMongo } = require('../db/connection')
 
 const PORT = process.env.PORT || 3000
 const start = async () => {
-  await connectMongo()
-  app.listen(PORT, (err) => {
-    if (err) {
-      console.error('Error at server lanch', err)
-    }
-    console.log(`Server running. Use our API on port: ${PORT}`)
-  })
+  try {
+    await connectMongo()
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.error('Error at server lanch', err)
+      }
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  } catch (err) {
+    console.error(`Faied to launch application with error ${err.message}`)
+  }
 }
 start()
