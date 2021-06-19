153d0087c8657cfc858e46a98faa00039fd83a04
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-19 22:06:33 +0300

    refactoring controllers (created Services)

--- conrollers/contactsControllers.js
+++ conrollers/contactsControllers.js
@@ -1,56 +1,37 @@
-// const {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact
-// } = require('../model')
-
-const { Contact } = require('../db/contactModel')
+const {
+  getContacts,
+  getContactById,
+  addContact,
+  changeContactById,
+  deleteContactById
+} = require('../services/contactsServices')
 
 module.exports = {
-  getContactsList: async (req, res, next) => {
-    const contacts = await Contact.find({})
-    // const contacts = await listContacts(req)
+  getContactsListController: async (req, res, next) => {
+    const contacts = await getContacts()
     res.json({ contacts })
   },
-  getContactById: async (req, res, next) => {
+  getContactByIdController: async (req, res, next) => {
     const { contactId } = req.params
-    const contact = await Contact.findById(contactId)
-    // const contact = await getContactById(contactId)
-    if (!contact) {
-      return res.status(404).json({ message: 'Not found' })
-    }
+    const contact = await getContactById(contactId)
     res.status(200).json({ contact })
   },
-  addContact: async (req, res, next) => {
-    const contact = new Contact({ ...req.body })
-    await contact.save()
-    // const body = req.body
-    // await addContact({ ...body })
+  addContactController: async (req, res, next) => {
+    const body = req.body
+    await addContact(body)
     res.status(201).json({ message: 'contact added to data base' })
   },
-  deleteContactById: async (req, res, next) => {
+  deleteContactByIdController: async (req, res, next) => {
     const { contactId } = req.params
-    await Contact.findByIdAndRemove(contactId)
-    // const deleted = await removeContact(contactId)
-    // if (!deleted) {
-    //   res.status(404).json({ message: 'Not found' })
-    //   return
-    // }
+    await deleteContactById(contactId)
     res.status(200).json({ message: 'contact deleted' })
   },
-  updateContactById: async (req, res, next) => {
+  updateContactByIdController: async (req, res, next) => {
     const { contactId } = req.params
     const body = req.body
-    await Contact.findByIdAndUpdate(contactId, { $set: { ...body } })
-    // console.log('Enter to updateContact')
-    // const updatedContact = await updateContact(contactId, req.body)
-    // if (!updatedContact) {
-    //   return res.status(404).json({ message: 'Not found' })
-    // }
+    await changeContactById({ contactId, body })
     res.status(200).json({
-      message: `Contact with id: ${contactId} update success`
+      message: `Contact with id: ${contactId} updated`
     })
   }
 }
