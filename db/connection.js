d6459b08d91e592dd9e193266dcf35849c669e54
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-19 16:36:37 +0300

    handle error connection

--- db/connection.js
+++ db/connection.js
@@ -6,13 +6,20 @@ const getCollections = () => {
 }
 
 const connectMongo = async () => {
-  const client = await MongoClient.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  const db = client.db('db-contacts')
+  try {
+    const client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    const db = client.db('db-contacts')
 
-  collections.Contacts = db.collection('contacts')
+    collections.Contacts = db.collection('contacts')
+    console.log('Database connection successful')
+  } catch (err) {
+    console.error(
+      `Failed to connect to data  base with error: "${err.message}"`
+    )
+  }
 }
 
 module.exports = { connectMongo, getCollections }
