2360693a6503fff2cfab0bc672bdc7cbee331508
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-19 16:37:30 +0300

    create patch contact validation

--- middlewares/validationMiddlewares.js
+++ middlewares/validationMiddlewares.js
@@ -21,5 +21,22 @@ module.exports = {
       })
     }
     next()
+  },
+  patchContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(30),
+      email: Joi.string().email({
+        minDomainSegments: 2
+      }),
+      phone: Joi.string().pattern(/^[(][0-9]{3}[)] [0-9]{3}-[0-9]{4}$/)
+    }).min(1)
+    const validationResult = schema.validate(req.body)
+    if (validationResult.error) {
+      const keyError = validationResult.error.details[0].context.key
+      return res.status(400).json({
+        message: `wrong ${keyError} field`
+      })
+    }
+    next()
   }
 }
