6717e587bbcc4672e4efe13ef0b96e5227dae8b6
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-26 23:49:17 +0300

    added auth routes

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
-
+const { authRouter } = require('./routes/api/authRouter')
 const contactsRouter = require('./routes/api/contacts')
 const { errorHandler } = require('./helpers/apiHelpers')
+
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -12,6 +13,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/contacts/users', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
