fe2ff5557ca522eccface30f7911eb722a370e67
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-26 23:48:12 +0300

    added and refactoring errors

--- helpers/errors.js
+++ helpers/errors.js
@@ -1,18 +1,46 @@
-class ValidationError extends Error {
+class noteBookError extends Error {
+  constructor(message) {
+    super(message)
+    this.status = 400
+  }
+}
+class ValidationError extends noteBookError {
   constructor(message) {
     super(message)
     this.status = 400
   }
 }
 
-class EmptyParametersError extends Error {
+class EmptyParametersError extends noteBookError {
   constructor(message) {
     super(message)
     this.status = 400
+  }
+}
+class NotAuthorisedError extends noteBookError {
+  constructor(message) {
+    super(message)
+    this.status = 400
+  }
+}
+class NotAuthanticateError extends noteBookError {
+  constructor(message) {
+    super(message)
+    this.status = 401
+  }
+}
+class RegistrationConflictError extends noteBookError {
+  constructor(message) {
+    super(message)
+    this.status = 409
   }
 }
 
 module.exports = {
+  noteBookError,
   ValidationError,
-  EmptyParametersError
+  EmptyParametersError,
+  NotAuthorisedError,
+  RegistrationConflictError,
+  NotAuthanticateError
 }
