9da052d8b7ac92a83c3f020b9871830ed10e2fc9
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-06-27 20:07:56 +0300

    added account creation limiter to authRouter

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -10,19 +10,17 @@ const { asyncWrapper } = require('../../helpers/apiHelpers')
 const {
   authorizationValidation
 } = require('../../middlewares/validationMiddlewares')
-
+const { createAccountLimiter } = require('../../helpers/rate-limit')
 const { authMiddleware } = require('../../middlewares/authMiddleware')
 
 router.post(
   '/signup',
+  createAccountLimiter,
   authorizationValidation,
   asyncWrapper(registrationController)
 )
-
 router.post('/login', authorizationValidation, asyncWrapper(loginController))
-
 router.post('/logout', authMiddleware, asyncWrapper(logoutController))
-
 router.get('/current', authMiddleware, asyncWrapper(getCurrentUserController))
 
 module.exports = { authRouter: router }
