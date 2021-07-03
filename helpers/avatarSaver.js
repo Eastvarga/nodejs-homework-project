a3aa9d19f4201e25adb88e56d3e895d54edbf699
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-03 21:22:11 +0300

    create helper avatar saver

---
+++ helpers/avatarSaver.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises
+const jimp = require('jimp')
+const path = require('path')
+const { AVATARS_DIR } = require('../helpers/uploads')
+const { v4: uuidv4 } = require('uuid')
+
+const avatarRenameAndSave = async (pathAvatar) => {
+  if (pathAvatar) {
+    const avatar = await jimp.read(pathAvatar)
+    await avatar
+      .autocrop()
+      .cover(
+        250,
+        250,
+        jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(pathAvatar)
+    const newPathWithExt = []
+    newPathWithExt.push(uuidv4())
+    newPathWithExt.push(pathAvatar.split('.')[1])
+    const avatarURL = path.join(AVATARS_DIR, newPathWithExt.join('.'))
+    await fs.rename(pathAvatar, avatarURL)
+    return avatarURL
+  }
+}
+
+module.exports = {
+  avatarRenameAndSave
+}
