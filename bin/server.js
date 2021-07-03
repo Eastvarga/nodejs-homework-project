38a8ba8408ab122e8b435e0692785a3df23cb38e
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-03 17:03:26 +0300

    added creators for upload and store avatars

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,29 @@
 const app = require('../app')
 const { connectMongo } = require('../db/connection')
 
+const fs = require('fs').promises
+const { UPLOAD_DIR, AVATARS_DIR } = require('../routes/api/upload')
+
+const isAccessible = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false)
+}
+
+const createFolderIsNotExist = async (folder) => {
+  if (!(await isAccessible(folder))) {
+    await fs.mkdir(folder)
+  }
+}
+
 const PORT = process.env.PORT || 3000
 const start = async () => {
   try {
     await connectMongo()
-    app.listen(PORT, (err) => {
+    app.listen(PORT, async (err) => {
+      await createFolderIsNotExist(UPLOAD_DIR)
+      await createFolderIsNotExist(AVATARS_DIR)
       if (err) {
         console.error('Error at server lanch', err)
       }
