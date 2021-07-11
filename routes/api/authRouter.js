e83b250bf90d4e29f4727c9e7920aea661cb39ed
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-11 13:11:42 +0300

    added verify and verify/verify/token routes

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -6,35 +6,51 @@ const {
   logoutController,
   getCurrentUserController,
   updateCurrentUserSubscriptionController,
-  updateCurrentUserAvatarController
+  updateCurrentUserAvatarController,
+  veryfyRegistrationController,
+  repeatedVeryfyRegistrationController,
 } = require('../../conrollers/authControllers')
 const { asyncWrapper } = require('../../helpers/apiHelpers')
 const {
   authorizationValidation,
-  subscriptionValidation
+  subscriptionValidation,
+  repeatedVerifyValidation,
 } = require('../../middlewares/validationMiddlewares')
 const { createAccountLimiter } = require('../../helpers/rate-limit')
 const { authMiddleware } = require('../../middlewares/authMiddleware')
 const { upload } = require('../../helpers/uploads')
+
 router.post(
   '/signup',
   createAccountLimiter,
   authorizationValidation,
   asyncWrapper(registrationController)
 )
+
 router.post('/login', authorizationValidation, asyncWrapper(loginController))
+
 router.post('/logout', authMiddleware, asyncWrapper(logoutController))
+
+router.post('/verify/', repeatedVerifyValidation,
+  asyncWrapper(repeatedVeryfyRegistrationController))
+
 router.get('/current', authMiddleware, asyncWrapper(getCurrentUserController))
+
+router.get('/verify/:verificationToken',
+  asyncWrapper(veryfyRegistrationController))
+
 router.patch(
   '/avatars',
   authMiddleware,
   upload.single('avatar'),
   asyncWrapper(updateCurrentUserAvatarController)
 )
+
 router.patch(
   '/',
   authMiddleware,
   subscriptionValidation,
   asyncWrapper(updateCurrentUserSubscriptionController)
 )
+
 module.exports = { authRouter: router }
